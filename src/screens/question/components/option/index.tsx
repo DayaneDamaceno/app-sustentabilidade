790bd41dcaf7aa373c74b4ec4ce5eae4547f60fd
Gu <gu.mivvi@gmail.com>
2023-11-12 14:47:28 -0300

    Update no arquivo de option

--- src/screens/question/components/option/index.tsx
+++ src/screens/question/components/option/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { RootStackParamList } from "../../../../routes";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { QuizOption } from "../../../../models/option";
 import { Level } from "../../../../models/level";
+import * as Font from 'expo-font';
 
 type NavigationProps = StackNavigationProp<RootStackParamList>;
 
@@ -23,19 +24,30 @@ export const Option: React.FC<OptionProps> = (props) => {
       lastAnswerWasCorrect: wasCorrect,
     });
   };
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  useEffect(() => {
+  async function loadFonts() {
+    await Font.loadAsync({
+      'Kanit-Regular': require('../../../../../assets/fonts/kanit/Kanit-Regular.ttf'),
+    });
+    setFontsLoaded(true);
+  }
+  
+  loadFonts();
+}, []);
 
   function onChoiceOption() {
     redirectToNextQuestion(props.option.isCorrect);
   }
-
   return (
     <>
       <TouchableOpacity style={styles.button} onPress={onChoiceOption}>
-        <Text style={styles.text}>{props.option.text}</Text>
+        <Text style={styles.text} >{props.option.text}</Text>
       </TouchableOpacity>
     </>
   );
 };
+
 const styles = StyleSheet.create({
   button: {
     backgroundColor: "#72D423",
@@ -47,13 +59,13 @@ const styles = StyleSheet.create({
     background: `linear-gradient(0deg, #72D423 0%, #72D423 100%), linear-gradient(0deg, #FF9A01 0%, #FF9A01 100%), #FF9A01`,
     boxShadow: `0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset`,
   },
-  text: {
+  text: { 
     color: "white",
     fontSize: 16,
-    //fontFamily: "Kanit",
+    fontFamily: 'Kanit-Regular',
     marginLeft: "5%",
     marginRight: "5%",
     marginVertical: "2%",
-    textAlign: "center",
+    textAlign: "center"
   },
 });
